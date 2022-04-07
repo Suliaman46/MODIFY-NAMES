e1d8bc8d66b2c283542945d99e10bc3b5753b5da
Suliaman Abass <suliaman.46@gmail.com>
2022-04-07 15:24:02 -0700

    INIT COMMIT

---
+++ modify.sh
@@ -0,0 +1,143 @@
+
+usage() { 
+
+cat<<EOT 1>&2
+Usage: 
+ $0 [-r] [-l|-u] <dir/file names>
+ $0 [-r] <sed pattern> <dir/file names>
+EOT
+exit 1;
+
+}
+
+sedValidityCheck() {
+	#run sed command on empty string
+	# print exit code and message
+	sedResult=$(echo "" | sed "$1" 2>&1)
+	exitCode=$?
+	
+	if [ $exitCode == 0 ]; then
+		return 0
+	else
+		echo "Fail, Message:'$sedResult', ErrorCode:'$exitCode'"
+	fi  
+
+}
+
+operation() {
+	#echo "recursive called"
+
+	if  [ -d "$1" ]; then
+
+		if [ $r == 1 ]; then
+			for file in "$1"/* ;do
+
+				operation "$file"
+			done
+		fi
+		updateName $1
+		
+		
+	elif [ -f "$1" ]; then
+
+		updateName "$1"
+	fi
+				   
+}
+
+updateName() {
+	fileName=`basename "$1"`
+	dirName=`dirname "$1"`
+	
+	if [ $l == 1 ]; then
+		#lowercase "$1"
+		temp=$(echo "$fileName" | tr 'A-Z' 'a-z')
+	elif [ $u == 1 ]; then
+		#uppercase "$1"
+		temp=$(echo "$fileName" | tr 'a-z' 'A-Z')
+	else
+		#sedPattern "$1"
+		temp=$(echo "$fileName" | sed "$SEDPATTERN" 2>&1)
+	fi
+	result="$dirName/$temp"
+	if [ -f "$result" ] || [ -d "$result" ]; then
+		echo "The "$result" name exists"
+	else
+	mv "$1" "$result"
+	echo "Changed from \""$1"\" to \""$result"\" "
+	fi
+
+}
+
+
+iterator() {
+
+	while [ -n "$1" ]; do
+	
+		if [ -f "$1" ]; then
+			updateName "$1"
+		elif [ -d "$1" ]; then
+			operation "$1" 
+		else
+			echo "The file "$1" does not exist"
+		fi
+		shift
+	done
+
+}
+
+
+r=0;l=0;u=0;
+
+while getopts ":rlu" o; do
+    case "${o}" in
+        r)
+            r=1
+            ;;
+        l)
+            l=1
+            ;;
+        u)
+            u=1
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+
+if [ $l == 1 ] && [ $u == 1 ]; then
+    usage
+
+elif [ $l == 0 ] && [ $u == 0 ]; then
+    #THE SED Pattern case
+    #echo "Sed"
+    if [ $# -ge 2 ]; then
+    	sedValidityCheck $1
+    	if [ $? == 0 ]; then
+    		echo "Succesful Sed Validity Check";
+    		SEDPATTERN="$1"
+    		shift
+    		#Figured out the sed pattern and it's validity 
+    		#Now on to the renaming
+
+		iterator "$@"
+        fi
+    else
+    	echo "Not Enough Arguments"
+    	usage
+    fi
+
+elif [ $l == 1 ] || [ $u == 1 ]; then
+
+	iterator $@
+    
+else
+    echo "lowercase"
+fi
+
+
+
+
---
+++ modify_examples.sh
@@ -0,0 +1,98 @@
+#!/bin/sh
+
+rm -r testing
+mkdir testing
+cd testing
+mkdir dir1
+mkdir dir2
+cd dir1
+touch file1
+touch file2
+mkdir dir11
+cd dir11
+touch file3.txt
+cd ../..
+cd dir2
+touch file4.txt
+touch file5.txt
+cd ..
+mkdir dir3
+cd dir3
+touch 'file with space'
+mkdir emptyDirectory
+touch oppexists
+touch OPPEXISTS
+cd ..
+
+echo "Before any Operation"
+ls -R  
+
+#1.
+echo "\n 1. Making all of dir1 uppercase recursively - 'modify.sh -r -u dir1' \n"
+
+bash ../modify.sh -r -u dir1
+echo "\nAfter Completion"
+ls -R
+
+
+#2. 
+echo "\n 2. Making file in dir2 and itself uppercase without recursion - 'modify.sh -u dir2/file4.txt dir2/file5.txt dir2' \n"
+
+bash ../modify.sh -u dir2/file4.txt dir2/file5.txt dir2
+echo "\nAfter Completion"
+
+echo "\n 2.1 Please note for this case the order is important and if the user attempted 'modify.sh -u dir2 dir2/file4.txt dir2/file5.txt' the name of the dir2 would first be changed to DIR2 and then the program would not be able to find dir2/file4.txt & dir2/file5.txt"
+ls -R
+
+#3
+echo "\n 3. Recursively making whole of DIR1 & DIR2 directories lowercase  - 'modify.sh -r -l DIR1 DIR2' \n"
+
+bash ../modify.sh -r -l DIR1 DIR2
+echo "\nAfter Completion"
+ls -R
+
+#4
+echo "\n 4. Recursively using sed to change names in dir2 - 'modify.sh -r s/file/mile/' \n"
+
+bash ../modify.sh -r s/file/mile/ dir2
+echo "\nAfter Completion"
+ls -R
+
+#5
+echo "\n 5. Attempting to change name but file already exists- 'modify.sh -l dir3/oppexists' \n"
+
+bash ../modify.sh -l dir3/oppexists
+echo "\nAfter Completion"
+ls -R
+
+#6
+echo "\n 6. Attempting to change name but file does not exist- 'modify.sh -l dir3/newFile' \n"
+
+bash ../modify.sh -l dir3/newFile
+echo "\nAfter Completion"
+ls -R
+
+#7
+echo "\n 7. Using sed to change file extensions recursively in the whole root (./) directory- '../modify.sh -r 's/\.txt/\.newext/' ./' \n"
+
+bash ../modify.sh -r 's/\.txt/\.newext/' ./
+echo "\nAfter Completion"
+ls -R
+
+#8
+echo "\n 8. Try to operate on empty directory - '../modify.sh -r -l dir3' \n"
+
+bash ../modify.sh -r -l dir3
+echo "\nAfter Completion"
+ls -R
+
+#9
+echo "\n 9. Try to operate on empty directory - '../modify.sh -r -l dir3' \n"
+
+bash ../modify.sh -r -l dir3
+echo "\nAfter Completion"
+ls -R
+
+
+
+
